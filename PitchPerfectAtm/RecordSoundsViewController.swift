24ca50b7fb8f416cf0e4f85120b969abbd389e6b
atm1504 <atm1504.in@gmail.com>
2021-06-20 19:19:37 +0530

    Wrong code

---
+++ PitchPerfectAtm/RecordSoundsViewController.swift
@@ -0,0 +1,111 @@
+//
+//  RecordSoundsViewController.swift
+//  PitchPerfectAtm
+//
+//  Created by Amartya Mondal on 18/06/21.
+//
+
+import UIKit
+import AVFoundation
+
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+    var recordingSession: AVAudioSession!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        stopRecordingButton.isEnabled=false
+        print("ViewDidLoad")
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        print("Permitted")
+                    } else {
+                        // failed to record!
+                        print("Not Permitted")
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        print("View will appear");
+    }
+    
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Recordingn in progress!")
+        // Handle the states of the buttons
+        recordButton.isEnabled=false
+        stopRecordingButton.isEnabled=true
+        recordingLabel.text="Recording in progress";
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory,.userDomainMask, true)[0] as String
+        let recordingName = "recordedVoice.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = URL(string: pathArray.joined(separator: "/"))
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        //        let session = AVAudioSession.sharedInstance()
+        //        try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
+        //
+        //        try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        //        audioRecorder.isMeteringEnabled = true
+        //        audioRecorder.prepareToRecord()
+        //        audioRecorder.record()
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: filePath!, settings: settings)
+            audioRecorder.isMeteringEnabled = true
+            
+            audioRecorder.delegate = self
+            audioRecorder.prepareToRecord()
+            audioRecorder.record()
+            
+            //            recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            //finishRecording(success: false)
+            print("Recording failed")
+        }
+        
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLabel.text="Tap to record"
+        recordButton.isEnabled=true
+        stopRecordingButton.isEnabled=false
+        
+        audioRecorder.stop()
+        audioRecorder = nil
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        
+        print("Recording has stopped")
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("Finished recording");
+    }
+    
+}
+
